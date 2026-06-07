a4a34753ee23b3899c8ecf7484725af92036a3e4
Akshay Shinde <akshayshinde281195@gmail.com>
2026-06-07 20:46:38 +0530

    adding new files

---
+++ src/main/java/com/example/SpringAIWorkshop/controller/AiController.java
@@ -0,0 +1,40 @@
+package com.example.SpringAIWorkshop.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.SpringAIWorkshop.service.AudioService;
+import com.example.SpringAIWorkshop.service.ChatService;
+import com.example.SpringAIWorkshop.service.ImageService;
+
+@RestController
+public class AiController {
+	
+	@Autowired
+	private ChatService chatservice;
+	
+	@Autowired
+	private AudioService audioService;
+	
+	@Autowired
+	private ImageService imageService;
+	
+	@GetMapping("/ask-ai")
+	public String askAi(@RequestParam String prompt) {
+		return chatservice.askAI(prompt);
+	}
+	
+	@GetMapping("/generate-audio")
+	public String generateAudio(@RequestParam String text) {
+		return audioService.convertTextToSpeech(text);
+	}
+	
+	@GetMapping("/generate-image")
+	public String generateImage(@RequestParam String prompt) {
+		return imageService.generateImage(prompt);
+	}
+	
+}
+
